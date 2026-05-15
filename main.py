fbefeeaab5c8e9954defaddb53a4665b479f7da4
Alexander Arvedson <alexander.arvedson@gmail.com>
2026-05-15 11:17:42 +0200

    greatly stripped down the codebase to work with a cleaner and more organized slate.

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from repo_intel.core import RepoIntel
-from context.builder import ContextBuilder
-
 
 def main():
-
-    repo = RepoIntel(
-        repo_path=".",
-        db_path="data/sqlite/repo.db"
-    )
-
-    builder = ContextBuilder(repo)
-
-    context = builder.build("generate_user_report")
-
-    print(context)
-
+    print("placeholder")
 
 if __name__ == "__main__":
     main()
